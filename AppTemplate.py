ad0480df1340ae2c93894d5175f3c355a5eb0f58
cheak <public@kueken.de>
2020-03-05 13:30:54 +0100

    etc

--- AppTemplate.py
+++ AppTemplate.py
@@ -1,5 +1,4 @@
 import core.webapp                                  # This is the REMI App
 
 if __name__ == '__main__':
-    core.webapp.startApp()
-
+    core.webapp.startApp()                          # Start the REMI App
