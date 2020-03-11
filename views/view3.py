c26d28916f320fa72c1ee7853372aaff3879344a
cheak <public@kueken.de>
2020-03-11 10:38:07 +0100

    Working on real Plant Visu Example.

--- views/view3.py
+++ views/view3.py
@@ -1,64 +1,55 @@
 import core.globals
-import time
-import random
-import threading
-from remi.gui import *
+import helpers.generatetable
+import remi.gui
 
-class Container(Container):
+# Just arrange freely the elements for fixed sceen sizes.
+
+class Container(remi.gui.Container):
 
     def __init__(self, AppInst=None, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.AppInst = AppInst
-        self.userInit(args, kwargs)
         self.constructUI()
-        self.style.update({'width': '100%', 'height': '100%', 'position': 'relative',
-                           'overflow': 'auto', 'margin': 'auto', 'border': '1px solid black',
-                           'box-sizing': 'border-box', 'padding': '10px', 'margin-top': '10px'})
-        # margin: auto centers the element, box-sizing: border-box forces padding settings just to be applied inside the box
+        self.userInit(args, kwargs)
 
 
     def constructUI(self):
-        schema = Image('/static:images/plant_view/visu_test.jpg', width='1500px')
-        self.append(schema, 'schema')
 
-        rl_temp = Label(f"{self.data['rl_temp']:.1f}°C", style={'position': 'absolute', 'top': '630px', 'left': '500px', 'background-color': 'green', 'font-weight': 'bold'})
-        vl_temp = Label(f"{self.data['vl_temp']:.1f}°C", style={'position': 'absolute', 'top': '790px', 'left': '500px', 'background-color': 'green', 'font-weight': 'bold'})
-        self.append(rl_temp, 'rl_temp')
-        self.append(vl_temp, 'vl_temp')
+        self.base = remi.gui.Container(width='1000px', height='900px',
+                                    style={'overflow': 'auto', 'margin': 'auto',
+                                           'border': '1px solid black',
+                                           'padding': '10px', 'margin-top': '10px'})
+        # margin: auto centers the element, class w3-display-container
 
-        # Add Button for Pump Control ON/OFF/AUTO
+        self.MyTestTable = helpers.generatetable.generateTable(tableName='tab', tableWidth='800px',
+                                                      noRows=4, noColumns=5,
+                                                      tableStyle={'table-layout': 'fixed', 'border': '1px solid red', 'overflow': 'hidden', 'word-wrap': 'break-word'},
+                                                      rowStyle={},
+                                                      cellStyle={'height': '25px', 'background-color': 'cyan'},
+                                                      testmode=False)
+
+        self.base.append(self.MyTestTable['tab'])
+
+        self.append(self.base)
+
+        self.MyTestTable['tab-2,5'].set_text('Lirum Larum Ipsum Dolrem sit Lirum Larum Ipsum Dolrem sitLirum Larum Ipsum Dolrem sitLirum Larum Ipsum Dolrem sitLirum Larum Ipsum Dolrem sit')
+
+        self.testbtn = remi.gui.Button('My Button')
+        self.MyTestTable['tab-1,3'].append(self.testbtn)
+
+        self.MyTestTable['tab-1,1'].style.update({'width': '10px'})
+        self.MyTestTable['tab-1,2'].style.update({'width': '20px'})
+        self.MyTestTable['tab-1,3'].style.update({'width': '40px'})
+        self.MyTestTable['tab-1,4'].style.update({'width': '80px'})
+        self.MyTestTable['tab-1,5'].style.update({'width': '160px'})
 
 
     def userInit(self, *args, **kwargs):
         self.shownInMenu = 'My Example Menu'
         self.menuTitle = 'Show View 3'
 
-        self.data = {}
-
-        # data Aquisition in own thread
-        t = threading.Thread(target=self.dummy_values)
-        t.daemon = True
-        t.start()
-
-
-
-
 
     def updateView(self):
-    # Update the UI controls with the new values if UI is idle
-
-        self.children['vl_temp'].set_text(f"{self.data['vl_temp']:.1f}°C")
-        self.children['rl_temp'].set_text(f"{self.data['rl_temp']:.1f}°C")
-
-
-
-    def dummy_values(self):
-    # Generate Random Values like data aquisition from Bus System
-
-        while True:
-            self.data['vl_temp'] = random.uniform(4.5, 8.5)
-            self.data['rl_temp'] = random.uniform(8.0, 12.0)
-            time.sleep(1.0)
-
-
+        # Here you can update the view if it needs updates
+        pass
 
