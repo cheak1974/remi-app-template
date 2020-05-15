a8919d78fd655be6748d7592822f07e23d71f531
Christian Kueken <public@kueken.de>
2020-05-15 21:32:58 +0200

    Added Host IP logging in case of reverse proxy configuration

--- helpers/connections.py
+++ helpers/connections.py
@@ -14,9 +14,17 @@ def handle_connections(AppInst=None):
         for session_id, app_inst in remi.server.clients.items():
             if session_id == AppInst.session:
                 for ws_client in app_inst.websockets:
-                    AppInst.logger.info(f'New Session with ID <{AppInst.session}> from host {ws_client.client_address}')
+                    headerlines = str(ws_client.headers).split('\n')         # Split header information along \n
+                    for header in headerlines:
+                        if header.find('X-Forwarded-For') >= 0:         # Header used by reverse Proxies to forward original host IP
+                            host = header.split(':')
+                            AppInst.logger.info(f'New Session with ID <{AppInst.session}> from host {host[1]}')
+                            connected_clients[AppInst.session] = host[1]
+                        else:
+                            AppInst.logger.info(f'New Session with ID <{AppInst.session}> from host {ws_client.client_address}')    # Host Information for direct connection
+                            connected_clients[AppInst.session] = ws_client.client_address
+
                     AppInst.logger.info(f'Session <{AppInst.session}> host headers: {ws_client.headers}')
-                    connected_clients[AppInst.session] = ws_client.client_address
                     connected_clients['number'] = connected_clients['number'] + 1
                     AppInst.logger.info(f'Connected clients ({connected_clients["number"]} in total): {connected_clients}')
 
